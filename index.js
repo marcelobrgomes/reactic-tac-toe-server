168ddd8416fbc042a6d3d9dfe4ad79eeb262b173
marcelobrgomes <marcelobrgomes@gmail.com>
2020-05-19 00:03:09 -0300

    Listening para o client

--- index.js
+++ index.js
@@ -6,10 +6,39 @@ app.get('/', (req,res)=> {
     res.sendFile(__dirname + '/index.html');
 })
 
+var gameRooms = [[]]
+
+function getNextRoom() {
+    var lastRoom = gameRooms[gameRooms.length-1]
+
+    if(lastRoom.length < 2) {
+        return lastRoom
+    } else {
+        gameRooms.push([])
+        return gameRooms[gameRooms.length-1]
+    }
+}
+
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id)
+    var room = getNextRoom()
+    room.push(socket.id)
+
+    console.log('room', room)
+
+    socket.on('move', gameData => {
+        if(room.length === 2) {
+            var to = room.indexOf(socket.id) === 0 ? room[1] : room[0]
+            
+            socket.to(to)
+            .emit('move', gameData)
+            console.log(gameData)
+        }
+    })
 
     socket.on('disconnect', () => {
+        room.splice(room.indexOf(socket.id))
+        console.log('room', room)
         console.log('user disconnected', socket.id)
     })
 })
