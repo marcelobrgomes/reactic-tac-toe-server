2e207d7d430366f80af90d9c2178c38eaf4837b7
marcelobrgomes <marcelobrgomes@gmail.com>
2020-06-25 20:12:09 -0300

    Controle de salas

--- index.js
+++ index.js
@@ -6,40 +6,81 @@ app.get('/', (req,res)=> {
     res.sendFile(__dirname + '/index.html');
 })
 
-var gameRooms = [[]]
+var rooms = []
 
-function getNextRoom() {
-    var lastRoom = gameRooms[gameRooms.length-1]
+const joinRoom = client => {
+    let room = getRoom();
 
-    if(lastRoom.length < 2) {
-        return lastRoom
-    } else {
-        gameRooms.push([])
-        return gameRooms[gameRooms.length-1]
-    }
+    let player = getRoomNextPlayer(room)
+    room.push({clientId: client.id, player: player})
+
+    return room;
 }
 
-io.on('connection', (socket) => {
-    console.log('a user connected', socket.id)
-    var room = getNextRoom()
-    room.push(socket.id)
+const getRoom = () => {
+    if(rooms.length === 0) {
+        rooms.push([])
+        return rooms[0]
+    }
 
-    console.log('room', room)
+    let nextIncompleteRoom = rooms.filter((room, i) => room.length < 2)
 
-    socket.on('move', gameData => {
-        if(room.length === 2) {
-            var to = room.indexOf(socket.id) === 0 ? room[1] : room[0]
-            
-            socket.to(to)
-            .emit('move', gameData)
-            console.log(gameData)
+    if(nextIncompleteRoom.length > 0) {
+        return nextIncompleteRoom[0]
+    }
+
+    rooms.push([])
+    return rooms[rooms.length -1]
+}
+
+const exitRoom = (clientId) => {
+    rooms.forEach(room => {
+        client = room.find(client => client.clientId === clientId)
+        if(client) {
+            room.splice(client, 1)
         }
     })
+}
 
-    socket.on('disconnect', () => {
-        room.splice(room.indexOf(socket.id))
-        console.log('room', room)
-        console.log('user disconnected', socket.id)
+const getRoomNextPlayer = (room) => {
+    if(room.length === 0) {
+        return 'X'
+    }
+
+    return room.find(client => client.player === 'X') ? 'O' : 'X'
+}
+
+io.on('connection', (client) => {
+    client.on('join', () => {
+        let room = joinRoom(client)
+        let roomPlayer = room.find(roomPlayer => roomPlayer.clientId === client.id);
+        
+        console.log('rooms', rooms)
+        console.log('current room', room)
+        client.emit('playerConnected', {
+            message: `Você é ${roomPlayer.player}. Aguardando oponente.`,
+            player: roomPlayer.player
+        })
+
+        client.broadcast.emit('playerConnected', {
+            message: roomPlayer.player === 'X' ? '' : `Sua vez`,
+            player: roomPlayer.player
+        })
+    })
+
+    client.on('play', (gameData) => {
+        client.broadcast.emit('updateBoard', {
+            gameArray: gameData.gameArray,
+            nextPlayer: gameData.nextPlayer,
+            message: 'Sua vez'
+        })
+    })
+
+    client.on('disconnect', () => {
+        console.log('user disconnected', client.id)
+        exitRoom(client.id)
+        // io.emit("update", room[client.id] + " se desconectou.");
+        
     })
 })
 
